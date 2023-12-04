ef371750beafddb54ef29af64010127dd54e35d2
ZAW YE YINT 2104795F <2104795F@student.tp.edu.sg>
2023-12-04 19:59:58 +0800

    TEST

---
+++ ProductReviewProject/Client/Static/Endpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+namespace ProductReviewProject.Client.Static
+{
+	public static class Endpoints
+	{
+		public static readonly string Prefix = "api";
+
+		public static readonly string ProductsEndpoint = $"{Prefix}/products";
+		public static readonly string ReviewsEndpoint = $"{Prefix}/reviews";
+		public static readonly string RatingsEndpoint = $"{Prefix}/ratings";
+		public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+		public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+
+
+
+
+	}
+}
